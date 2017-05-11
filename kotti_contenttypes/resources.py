ee8c2646c8b2d87ed60e3a369494fc9fa425f263
Oshane Bailey <b4.oshany@gmail.com>
2017-05-11 12:02:56 -0500

    Upgrade to 1.0.8

--- kotti_contenttypes/resources.py
+++ kotti_contenttypes/resources.py
@@ -6,12 +6,13 @@ Created on 2016-10-18
 """
 
 from kotti.interfaces import IDefaultWorkflow
-from kotti.resources import Content
+from kotti.resources import Document
 from sqlalchemy import Column
 from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
 from sqlalchemy import Unicode
 from zope.interface import implements
+from kotti_pdf.resources import PDF
 
 from kotti_contenttypes import _
 
@@ -21,9 +22,9 @@ class Folder(Content):
 
     implements(IDefaultWorkflow)
 
-    id = Column(Integer, ForeignKey('contents.id'), primary_key=True)
+    id = Column(Integer, ForeignKey('documents.id'), primary_key=True)
 
-    type_info = Content.type_info.copy(
+    type_info = Document.type_info.copy(
         name=u'Folder',
         title=_(u'Folder'),
         add_view=u'add_folder',
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ try:
 except IOError:
     CHANGES = ''
 
-version = '0.1.7'
+version = '0.1.8'
 
 install_requires = [
     'Kotti>=1.0.0',
